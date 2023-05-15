93901b29d91aee654ed52f4f887a0e1ec73187a3
rajutamanglama65 <raju@fusemachines.com>
2023-05-15 14:12:54 +0545

    chore: project started

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Video chat app</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv")
+
+dotenv.config()
+
+const app = express();
+const server = require("http").createServer(app);
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.use(cors());
+
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from the server side...");
+});
+
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id);
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callended");
+  });
+
+  socket.on("calluser", ({ userToCall, signalData, from, name }) => {
+    io.to(userToCall).emit("calluser", { signal: signalData, from, name });
+  });
+
+  socket.on("answercall", (data) => {
+    io.to(data.to).emit("callaccepted", data.signal);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
