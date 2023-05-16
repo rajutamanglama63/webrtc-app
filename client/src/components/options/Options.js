8ed9ddb1360d3bc9d770fb128117b57ee9ee34cd
rajutamanglama65 <raju@fusemachines.com>
2023-05-16 11:06:39 +0545

    feat: option fields created

--- client/src/components/options/Options.js
+++ client/src/components/options/Options.js
@@ -1,7 +1,110 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import {
+  Grid,
+  Typography,
+  Paper,
+  Container,
+  TextField,
+  Button,
+} from "@mui/material";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { useTheme } from "@mui/material/styles";
+import { Assignment, PhoneDisabled, Phone } from "@mui/icons-material";
+import { SocketContext } from "../../context/SocketContext";
 
 const Options = ({ children }) => {
-  return <div>{children}</div>;
+  const { me, callAccepted, name, setName, callEnded, leaveCall, callUser } =
+    useContext(SocketContext);
+  const [idToCall, setIdToCall] = useState("");
+
+  const theme = useTheme();
+
+  return (
+    <Container
+      sx={{
+        width: "600px",
+        margin: "35px 0",
+        padding: 0,
+        [theme.breakpoints.down("xs")]: {
+          width: "80%",
+        },
+      }}
+    >
+      <Paper
+        elevation={10}
+        sx={{ padding: "10px 20px", border: "2px solid black" }}
+      >
+        <form style={{ padding: "10px 20px", border: "2px solid black" }}>
+          <Grid
+            container
+            sx={{
+              width: "100%",
+              [theme.breakpoints.down("xs")]: {
+                flexDirection: "column",
+              },
+            }}
+          >
+            <Grid xs={12} md={6} sx={{ padding: "20px" }}>
+              <Typography gutterBottom variant="h6">
+                Account Info
+              </Typography>
+              <TextField
+                label="Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                fullWidth
+              />
+              <CopyToClipboard text={me} sx={{ marginTop: "20px" }}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                  startIcon={<Assignment fontSize="large" />}
+                >
+                  Copy Your ID
+                </Button>
+              </CopyToClipboard>
+            </Grid>
+            <Grid item xs={12} md={6} sx={{ padding: "20px" }}>
+              <Typography gutterBottom variant="h6">
+                Make a call
+              </Typography>
+              <TextField
+                label="ID to call"
+                value={idToCall}
+                onChange={(e) => setIdToCall(e.target.value)}
+                fullWidth
+              />
+              {callAccepted && !callEnded ? (
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  startIcon={<PhoneDisabled fontSize="large" />}
+                  fullWidth
+                  onClick={leaveCall}
+                  sx={{ marginTop: "20px" }}
+                >
+                  Hang Up
+                </Button>
+              ) : (
+                <Button
+                  variant="contained"
+                  color="primary"
+                  startIcon={<Phone fontSize="large" />}
+                  fullWidth
+                  onClick={() => callUser(idToCall)}
+                  sx={{ marginTop: "20px" }}
+                >
+                  Call
+                </Button>
+              )}
+            </Grid>
+          </Grid>
+        </form>
+        {children}
+      </Paper>
+    </Container>
+  );
 };
 
 export default Options;
--- client/src/components/videoPlayer/VideoPlayer.js
+++ client/src/components/videoPlayer/VideoPlayer.js
@@ -1,103 +1,103 @@
-import React, { useContext } from "react";
+// import React, { useContext } from "react";
 // // import ReactPlayer from "react-player";
 import { SocketContext } from "../../context/SocketContext";
-import "./videoPlayer.css";
-
-const VideoPlayer = () => {
-  const { name, callAccepted, myVideoRef, userVideo, callEnded, stream, call } =
-    useContext(SocketContext);
-  return (
-    <div className="video-container">
-      <div className="stream-container">
-        {/* my own video */}
-        {stream && (
-          <div>
-            <p className="username">{name || "Name"}</p>
-            <video
-              playsInline
-              muted
-              ref={myVideoRef}
-              autoPlay
-              className="stream-box"
-            />
-          </div>
-        )}
-
-        {/* user video */}
-        {callAccepted && !callEnded && (
-          <div>
-            <p className="username">{call.name || "Name"}</p>
-            <video
-              playsInline
-              ref={userVideo}
-              autoPlay
-              className="stream-box"
-            />
-          </div>
-        )}
-      </div>
-    </div>
-  );
-};
-
-export default VideoPlayer;
-
-// import React, { useContext } from "react";
-// import { Grid, Typography, Paper } from "@mui/material";
+// import "./videoPlayer.css";
 
 // const VideoPlayer = () => {
 //   const { name, callAccepted, myVideoRef, userVideo, callEnded, stream, call } =
 //     useContext(SocketContext);
-
-//   console.log("my-vid: ", myVideoRef);
 //   return (
-//     <Grid
-//       container
-//       sx={{
-//         justifyContent: "center",
-//       }}
-//     >
-//       {stream && (
-//         <Paper
-//           sx={{ padding: "10px", border: "2px solid black", margin: "10px" }}
-//         >
-//           <Grid item xs={12} md={6}>
-//             <Typography variant="h5" gutterBottom>
-//               {name || "Name"}
-//             </Typography>
+//     <div className="video-container">
+//       <div className="stream-container">
+//         {/* my own video */}
+//         {stream && (
+//           <div>
+//             <p className="username">{name || "Name"}</p>
 //             <video
 //               playsInline
 //               muted
 //               ref={myVideoRef}
-//               autoPlay={true}
-//               style={{ width: "550px" }}
+//               autoPlay
+//               className="stream-box"
 //             />
-//           </Grid>
-//         </Paper>
-//       )}
-//       {callAccepted && !callEnded && (
-//         <Paper
-//           className={{
-//             padding: "10px",
-//             border: "2px solid black",
-//             margin: "10px",
-//           }}
-//         >
-//           <Grid item xs={12} md={6}>
-//             <Typography variant="h5" gutterBottom>
-//               {call.name || "Name"}
-//             </Typography>
+//           </div>
+//         )}
+
+//         {/* user video */}
+//         {callAccepted && !callEnded && (
+//           <div>
+//             <p className="username">{call.name || "Name"}</p>
 //             <video
 //               playsInline
 //               ref={userVideo}
-//               autoPlay={true}
-//               style={{ width: "550px" }}
+//               autoPlay
+//               className="stream-box"
 //             />
-//           </Grid>
-//         </Paper>
-//       )}
-//     </Grid>
+//           </div>
+//         )}
+//       </div>
+//     </div>
 //   );
 // };
 
 // export default VideoPlayer;
+
+import React, { useContext } from "react";
+import { Grid, Typography, Paper } from "@mui/material";
+
+const VideoPlayer = () => {
+  const { name, callAccepted, myVideoRef, userVideo, callEnded, stream, call } =
+    useContext(SocketContext);
+
+  console.log("my-vid: ", myVideoRef);
+  return (
+    <Grid
+      container
+      sx={{
+        justifyContent: "center",
+      }}
+    >
+      {stream && (
+        <Paper
+          sx={{ padding: "10px", border: "2px solid black", margin: "10px" }}
+        >
+          <Grid item xs={12} md={6}>
+            <Typography variant="h5" gutterBottom>
+              {name || "Name"}
+            </Typography>
+            <video
+              playsInline
+              muted
+              ref={myVideoRef}
+              autoPlay={true}
+              style={{ width: "550px" }}
+            />
+          </Grid>
+        </Paper>
+      )}
+      {callAccepted && !callEnded && (
+        <Paper
+          className={{
+            padding: "10px",
+            border: "2px solid black",
+            margin: "10px",
+          }}
+        >
+          <Grid item xs={12} md={6}>
+            <Typography variant="h5" gutterBottom>
+              {call.name || "Name"}
+            </Typography>
+            <video
+              playsInline
+              ref={userVideo}
+              autoPlay={true}
+              style={{ width: "550px" }}
+            />
+          </Grid>
+        </Paper>
+      )}
+    </Grid>
+  );
+};
+
+export default VideoPlayer;
